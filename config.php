05a76bc49f879caa8a0971b9ce043b8a69a32a62
Nico <nicof66@gmail.com>
2025-05-15 23:05:41 +0200

    Mise en page + hebergement OK ionos + planetHoster

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php
 
-$DB_HOST = 'sql300.infinityfree.com';
-$DB_USER = 'if0_38909992';
-$DB_PORT = 3306;
-$DB_PASS = 'Darts66540';
-$DB_NAME = 'if0_38909992_darts';
+$DB_HOST = 'localhost';
+$DB_USER = 'fvasnmcf_root';
+// $DB_PORT = 3306;
+$DB_PASS = 'Darts66540!';
+$DB_NAME = 'fvasnmcf_darts-games';
 
-$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME,$DB_PORT);
+$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
-if ($mysqli->connect_error) {
+
+if ($mysqli->connect_error) {    
     die("Échec de la connexion à la base de données : " . $mysqli->connect_error);
 }else {    
 }
